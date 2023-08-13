9bee013817239ca07a0f08290a75daa7605eb32e
rafaelsantiago <rafael.santiago.silva.1405@gmail.com>
2023-08-13 16:13:33 -0300

    Atualizei o exercício de media query

--- EX52-Hamburguer/script.js
+++ EX52-Hamburguer/script.js
@@ -1,8 +1,17 @@
+var menu = document.querySelector("menu");
+
 function clickMenu(){
-    let menu = document.querySelector("menu");
     if(menu.style.display == "block"){
         menu.style.display = "none";
     }else{
         menu.style.display = "block";
+    }
+}
+
+function mudouTamanho(){
+    if(window.innerWidth >= 992){
+        menu.style.display = "block";
+    }else{
+        menu.style.display = "none";
     }
 }
