0785beb7c47a058bd78d05831746889436d5c9fb
rafaelsantiago <rafael.santiago.silva.1405@gmail.com>
2023-08-13 16:00:22 -0300

    Criação dos arquivos principais para mobile first.
    Criação do conteúdo, estilo e menu hamburguer.

---
+++ EX52-Hamburguer/script.js
@@ -0,0 +1,8 @@
+function clickMenu(){
+    let menu = document.querySelector("menu");
+    if(menu.style.display == "block"){
+        menu.style.display = "none";
+    }else{
+        menu.style.display = "block";
+    }
+}
